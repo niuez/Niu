d4677ab54bd198d29a7084ada0e9fec50b6d15af
niuez <niuez.graph@gmail.com>
2020-12-16 01:22:01 +0900

    [add] failure test for ord

--- src/expression2.rs
+++ src/expression2.rs
@@ -379,3 +379,9 @@ fn parse_conditions_test() {
 fn parse_all_test() {
     println!("{:?}", parse_expression("1 + 2 == 3 * 4 || 5 << 6 & 7 >> 8 != 9 | 0 ^ 1 && 2 % 3 < 4 / 5 && 6 > 7 && 8 < 9 || 0 <= 1 && 2 >= 3"));
 }
+
+#[test]
+#[should_panic]
+fn parse_conditions_failure_test() {
+    println!("{:?}", parse_expression("1 == 2 == 3"));
+}
