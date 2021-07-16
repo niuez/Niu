9c8091c285fe1cac435bfb1fb266178f6857c5a5
niuez <niuez.graph@gmail.com>
2021-07-16 15:34:44 +0900

    [add] variables info for mutable check

--- src/mut_checker.rs
+++ src/mut_checker.rs
@@ -1,5 +1,34 @@
+use std::collections::HashMap;
 use crate::trans::TypeAnnotation;
+use crate::unary_expr::Variable;
 
 trait MutCheck {
-    fn mut_check(&self, ta: &TypeAnnotation) -> Result<(), String>;
+    fn mut_check(&self, ta: &TypeAnnotation, vars: &mut VariablesInfo) -> Result<(), String>;
+}
+
+pub struct VariablesInfo {
+    is_mut: Vec<HashMap<Variable, bool>>,
+}
+
+impl VariablesInfo {
+    pub fn new() -> Self {
+        Self { is_mut: Vec::new() }
+    }
+    pub fn into_scope(&mut self) {
+        self.is_mut.push(HashMap::new());
+    }
+    pub fn out_scope(&mut self) {
+        self.is_mut.pop();
+    }
+    pub fn regist_variable(&mut self, var: &Variable, is_mut: bool) {
+        self.is_mut.last_mut().unwrap().insert(var.clone(), is_mut);
+    }
+    pub fn find_variable(&self, var: &Variable) -> Result<bool, String> {
+        for is_mut in self.is_mut.iter().rev() {
+            if let Some(res) = is_mut.get(var) {
+                return Ok(*res);
+            }
+        }
+        Err(format!("not found variable {:?}", var))
+    }
 }
