5fca1286610641d0e3da8df277273140bf08ae3a
niuez <niuez.graph@gmail.com>
2021-11-02 16:05:24 +0900

    [add] variable_declaration

--- src/let_declaration.rs
+++ src/let_declaration.rs
@@ -4,6 +4,8 @@ use nom::character::complete::*;
 use nom::sequence::*;
 use nom::bytes::complete::*;
 use nom::combinator::*;
+use nom::multi::*;
+use nom::branch::*;
 
 use crate::identifier::{ Identifier, parse_identifier };
 use crate::expression::{ Expression, parse_expression };
@@ -15,12 +17,86 @@ use crate::move_checker::*;
 use crate::type_spec::*;
 
 #[derive(Debug)]
+pub enum VariableDeclaration {
+    Leaf(Identifier, bool),
+    Tuple(Vec<VariableDeclaration>),
+}
+
+impl GenType for VariableDeclaration {
+    fn gen_type(&self, equs: &mut TypeEquations, trs: &TraitsInfo) -> TResult {
+        match self {
+            VariableDeclaration::Leaf(id, is_mut) => {
+                let alpha = id.generate_not_void_type_variable("LetType", 0, equs);
+                equs.regist_variable(Variable::from_identifier(id.clone()), alpha.clone());
+                Ok(alpha)
+            }
+            VariableDeclaration::Tuple(vars) => {
+                let params = vars.iter().map(|v| v.gen_type(equs, trs)).collect::<Result<Vec<_>, _>>()?;
+                Ok(Type::Tuple(params))
+            }
+        }
+    }
+}
+
+impl MutCheck for VariableDeclaration {
+    fn mut_check(&self, ta: &TypeAnnotation, vars: &mut VariablesInfo) -> Result<MutResult, String> {
+        match self {
+            VariableDeclaration::Leaf(id, is_mut) => {
+                vars.regist_variable(id, *is_mut);
+                if ta.annotation(id.get_tag_number(), "LetType", 0).is_reference() && *is_mut {
+                    Err(format!("Reference cant be mutable, {:?}", id))
+                }
+                else {
+                    Ok(MutResult::NoType)
+                }
+            }
+            VariableDeclaration::Tuple(params) => {
+                for v in params.iter() {
+                    v.mut_check(ta, vars)?;
+                }
+                Ok(MutResult::NoType)
+            }
+        }
+    }
+}
+
+impl VariableDeclaration {
+    fn regist_vars(&self, mc: &mut VariablesMoveChecker) {
+        match self {
+            VariableDeclaration::Leaf(id, is_mut) => {
+                mc.regist_var(id);
+            }
+            VariableDeclaration::Tuple(params) => {
+                for v in params.iter() {
+                    v.regist_vars(mc);
+                }
+            }
+        }
+    }
+}
+
+fn parse_declaration_tuple(s: &str) -> IResult<&str, VariableDeclaration> {
+    let (s, (_, _, tuples, _, _, _, _)) = tuple((char('('), multispace0, separated_list1(tuple((multispace0, char(','), multispace0)), parse_variable_declaration), multispace0, opt(char(',')), multispace0, char(')')))(s)?;
+    Ok((s, VariableDeclaration::Tuple(tuples)))
+}
+
+fn parse_declration_leaf(s: &str) -> IResult<&str, VariableDeclaration> {
+    let (s, (is_mut, id)) = tuple((opt(tuple((tag("mut"), multispace1))), parse_identifier))(s)?;
+    Ok((s, VariableDeclaration::Leaf(id, is_mut.is_some())))
+}
+
+pub fn parse_variable_declaration(s: &str) -> IResult<&str, VariableDeclaration> {
+    alt((parse_declration_leaf, parse_declaration_tuple))(s)
+}
+
+#[derive(Debug)]
 pub struct LetDeclaration {
     pub id: Identifier,
     pub is_mut: bool,
     pub type_info: Option<TypeSpec>,
     pub value: Expression,
 }
+
 
 impl GenType for LetDeclaration {
     fn gen_type(&self, equs: &mut TypeEquations, trs: &TraitsInfo) -> TResult {
